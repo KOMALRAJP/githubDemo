313e03345f7d517ba0ac73c7da3cef7f5f9d9e06
shree <komal.rajput7@gmail.com>
2023-01-23 18:22:34 +0530

    First comment

---
+++ gitprojecttest/src/gitprojecttest/test1.java
@@ -0,0 +1,19 @@
+package gitprojecttest;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 for(int i=2;i<=10;i++)
+		  {
+			  for(int j=1;j<=10;j++)
+			  { 
+				//  System.out.println(" "+i*+j+" ");
+				  System.out.format(" %d", i * j);
+			  }
+				System.out.println();
+		  }
+
+	}
+
+}
